e7c290a7b4a9df4fa631b7ed8e2635981e78feeb
lampouras <lampouras@DCS03780.windows.dcs.shef.ac.uk>
2016-01-11 18:40:19 +0000

    Added similarity measure methods

--- src/main/java/similarity_measures/Levenshtein.java
+++ src/main/java/similarity_measures/Levenshtein.java
@@ -44,7 +44,7 @@ public class Levenshtein {
         double similarity = 1.0 - ((double) d[s1Length][s2Length] / (double) maxLength);
         return similarity;
     }
-    
+
     public static double getSimilarity(String s1, String s2, boolean perChar, boolean greek) {
         if (greek) {
             s1 = StringManipulation.normalizeGreek(s1);
@@ -128,7 +128,7 @@ public class Levenshtein {
             return similarity;
         }
     }
-    
+
     public static double getNormDistance(String s1, String s2, int refSize) {
         s1 = s1.toUpperCase().replaceAll("\\s", " ");
         s2 = s2.toUpperCase().replaceAll("\\s", " ");
@@ -145,10 +145,10 @@ public class Levenshtein {
         if (s2Length == 0) {
             return 1.0;
         }
-        
+
         return getNormDistance(s1Tokens, s2Tokens, (double) refSize * 2);
     }
-    
+
     public static double getNormDistance(String s1, String s2) {
         s1 = s1.toUpperCase().replaceAll("\\s", " ");
         s2 = s2.toUpperCase().replaceAll("\\s", " ");
@@ -165,14 +165,14 @@ public class Levenshtein {
         if (s2Length == 0) {
             return 1.0;
         }
-        
+
         return getNormDistance(s1Tokens, s2Tokens, (double) (1));
     }
-    
+
     public static double getNormDistance(ArrayList<String> s1Tokens, ArrayList<String> s2Tokens, double normSizeDenum) {
         int s1Length = s1Tokens.size();
         int s2Length = s2Tokens.size();
-        
+
         int[][] d = new int[s1Length + 1][s2Length + 1];
 
         for (int i = 0; i <= s1Length; i++) {
@@ -193,9 +193,9 @@ public class Levenshtein {
                 d[i][j] = MathFunctions.minOf3(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost);
             }
         }
-        return (double) (((double) d[s1Length][s2Length])/ (normSizeDenum));
+        return (double) (((double) d[s1Length][s2Length]) / (normSizeDenum));
     }
-    
+
     public static int getDistance(String s1, String s2) {
         s1 = s1.toUpperCase().replaceAll("\\s", " ");
         s2 = s2.toUpperCase().replaceAll("\\s", " ");
@@ -229,7 +229,7 @@ public class Levenshtein {
 
         return d[s1Length][s2Length];
     }
-    
+
     //CAN ONLY ADD, NOT DELERE ARE SUBSTITUTE    
     public static double getIncreasingDistance(String s1, String s2) {
         s1 = s1.toUpperCase().replaceAll("\\s", " ");
@@ -271,7 +271,7 @@ public class Levenshtein {
 
         return (double) ((double) d[s1Length][s2Length] / (double) (s1Length + s2Length));
     }
-    
+
     public static double getSimilarity(String s1, String s2) {
         s1 = s1.toLowerCase().replaceAll("\\s", " ");
         s2 = s2.toLowerCase().replaceAll("\\s", " ");
@@ -285,7 +285,7 @@ public class Levenshtein {
         if (s2Length == 0) {
             return 0.0;
         }
-        
+
         boolean pass = false;
         if (s1Length < 2 || s2Length < 2) {
             pass = true;
@@ -293,7 +293,7 @@ public class Levenshtein {
         for (int i = 1; i <= s1Length - 1; i++) {
             if (!pass) {
                 String sub1 = s1.substring(i - 1, i + 1);
-                for (int j = 1; j <= s2Length - 1; j++) {                
+                for (int j = 1; j <= s2Length - 1; j++) {
                     if (!pass) {
                         String sub2 = s2.substring(j - 1, j + 1);
                         if (sub1.equalsIgnoreCase(sub2)) {
@@ -332,7 +332,7 @@ public class Levenshtein {
         }
         return 0.0;
     }
-    
+
     public static Object[] getMatchedSimilarity(ArrayList<String> s1Tokens, ArrayList<String> s2Tokens, ArrayList<Double> tokenImportance) {
         if (s1Tokens.isEmpty() || s2Tokens.isEmpty()) {
             Object[] results = new Object[4];
@@ -342,20 +342,20 @@ public class Levenshtein {
             results[3] = -1;
             return results;
         }
-        
+
         ArrayList<Double> matchImportance = new ArrayList<Double>();
         for (String token : s2Tokens) {
             matchImportance.add(1.0);
         }
         HashMap<int[], Double> matches = new HashMap<int[], Double>();
-        int maxLength = (s1Tokens.size() > s2Tokens.size())? s1Tokens.size(): s2Tokens.size();
-        
+        int maxLength = (s1Tokens.size() > s2Tokens.size()) ? s1Tokens.size() : s2Tokens.size();
+
         HashSet<Integer> matchedIndeces1 = new HashSet<Integer>();
         HashSet<Integer> matchedIndeces2 = new HashSet<Integer>();
         for (int m = 0; m < maxLength; m++) {
             int[] maxMatch = null;
             double maxSimilarity = -1.0;
-            for (int i = 0; i < s1Tokens.size(); i++) {                
+            for (int i = 0; i < s1Tokens.size(); i++) {
                 if (!matchedIndeces1.contains(i)) {
                     String token1 = s1Tokens.get(i);
                     for (int j = 0; j < s2Tokens.size(); j++) {
@@ -364,7 +364,7 @@ public class Levenshtein {
 
                             double similarity;
                             if (token1.equalsIgnoreCase("$num$") || token2.equalsIgnoreCase("$num$")) {
-                                similarity = (token1.equalsIgnoreCase(token2))? 1.0: 0.0;
+                                similarity = (token1.equalsIgnoreCase(token2)) ? 1.0 : 0.0;
                             } else {
                                 similarity = getSimilarity(token1, token2);
                             }
@@ -380,44 +380,44 @@ public class Levenshtein {
                     }
                 }
             }
-            if (maxSimilarity >= 0.0 && maxMatch != null) {                
+            if (maxSimilarity >= 0.0 && maxMatch != null) {
                 matchedIndeces1.add(maxMatch[0]);
                 matchedIndeces2.add(maxMatch[1]);
                 matches.put(maxMatch, maxSimilarity);
-                
+
                 Double importance = 0.25;
                 if (maxMatch[0] < tokenImportance.size()) {
                     importance = tokenImportance.get(maxMatch[0]);
                 }
-                
+
                 matchImportance.set(maxMatch[1], importance);
             }
         }
-        
+
         double totalSimilarity = 0.0;
         /*for (int[] match : matches.keySet()) {
-            double weight = entityFrequencies.getEntityNameImportance().get(match[0]);
-            Double similarity = matches.get(match);
+         double weight = entityFrequencies.getEntityNameImportance().get(match[0]);
+         Double similarity = matches.get(match);
             
-            totalSimilarity += weight * similarity;
-        }*/
+         totalSimilarity += weight * similarity;
+         }*/
         for (Double similarity : matches.values()) {
             totalSimilarity += similarity;
         }
-        double finalSimilarity = totalSimilarity/(double) maxLength;
-        
+        double finalSimilarity = totalSimilarity / (double) maxLength;
+
         double matchCrosses = 0.0;
         for (int[] match1 : matches.keySet()) {
             for (int[] match2 : matches.keySet()) {
                 if (!Arrays.equals(match1, match2)) {
-                    if ((match1[0] < match2[0] && match1[1] > match2[1]) ||
-                            (match1[0] > match2[0] && match1[1] < match2[1])) {
+                    if ((match1[0] < match2[0] && match1[1] > match2[1])
+                            || (match1[0] > match2[0] && match1[1] < match2[1])) {
                         matchCrosses += 1.0;
                     }
                 }
             }
         }
-        
+
         //System.out.println(s1Tokens + "<>" + s2Tokens + " = " + finalSimilarity);
         Object[] results = new Object[4];
         results[0] = finalSimilarity;
@@ -517,5 +517,9 @@ public class Levenshtein {
         }
 
         return (double) d[s1Length][s2Length];
+    }
+
+    public void test() {
+
     }
 }
